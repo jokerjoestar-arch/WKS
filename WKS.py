9c029a5ba8b8974f4d5a6e1e983462c838dfe210
Joker_Joestar166 <48660824+jokerjoestar-arch@users.noreply.github.com>
2022-08-11 17:43:57 -0400

    Add files via upload

--- WKS.py
+++ WKS.py
@@ -1,12 +1,14 @@
 import wikipedia as wp
 
-#languaje def:
-wp.set_lang("en")
+languaje = open('languaje.txt', 'r')
+lang = languaje.read()
+wp.set_lang(lang)
 
-search=input("$")
+search=input("\n$")
 
 rese=wp.search(search, results=3)
 resu=wp.summary(search)
 
+print("\n")
 print(rese)
 print(resu)
